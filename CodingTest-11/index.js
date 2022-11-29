c5e72f006a0aa14caa99f84fe16c3db0ac44bf54
WonDuJin <windyjan1@naver.com>
2022-11-29 14:44:46 +0900

    CodingTest-11 txt파일 합쳐서 html브라우저에 출력하기

--- CodingTest-11/index.js
+++ CodingTest-11/index.js
@@ -36,6 +36,7 @@ const server  = http.createServer((req,res)=>{
       case "/":
         staticRoute('./html.txt', 200, 'text/html');
         break
+        // 3차전 결과보기 버튼을 클릭하면 나타나도록 설정
       case "/soccer":        
           staticRoute("./soccer.txt",200,"text/html");     
         break;
@@ -54,7 +55,7 @@ const server  = http.createServer((req,res)=>{
           staticRoute("./postHTML.txt",200,"text/html");  
         })
         break;
-
+      //컬러를 변경! (그런데 컬러가 잠깐만 바뀌고 다시 원상복귀되는 현상이 있음. 아마도 changeColor txt에서 div의 스타일 값이 바뀌지 않아서 다시 원상복구 되는 것 같음)
       case "/color":
         let changeColor = ''
         req.on('data',(data)=>{
